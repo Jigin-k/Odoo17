cb8471a1c6283909eebbc1f597c74f40ca47d243
cybrosys <jiginkarat03@gmail.com>
2024-09-16 19:38:12 +0530

    qqqqq

---
+++ website_bom/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website BOM",
+    'description': """
+Bom of ultiple products """,
+    'depends': ['base','website','mrp'],
+
+    'data': [
+        'views/bom_cart_views.xml',
+        'views/cart_inherit.xml'
+    ]
+}
