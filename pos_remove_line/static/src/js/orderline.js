304a839b9bd246c09707e95e5a8ee43413705d1f
cybrosys <jiginkarat03@gmail.com>
2024-09-25 17:51:23 +0530

    integration

--- pos_remove_line/static/src/js/orderline.js
+++ pos_remove_line/static/src/js/orderline.js
@@ -2,7 +2,6 @@
 
 import { Orderline } from "@point_of_sale/app/generic_components/orderline/orderline";
 import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
-
 import { patch } from "@web/core/utils/patch";
 
 patch(Orderline.prototype, {
@@ -15,8 +14,14 @@ patch(Orderline.prototype, {
 
     }
 });
-//patch(ProductScreen.prototype, {
-//    ClearAll() {
-//     const order = this.env.services.pos.get_order();
-//    }
-//});
+patch(ProductScreen.prototype, {
+    ClearAll(){
+    const order = this.env.services.pos.get_order();
+
+    if (order.orderlines.length > 0) {
+      for (let i = order.orderlines.length - 1; i >= 0; i--) {
+        order.removeOrderline(order.orderlines.at(i));
+      }
+    }
+  },
+});
