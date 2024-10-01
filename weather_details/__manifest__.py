46bc2f66735f5dcb1c1f59cf9d3bf888667d0291
cybrosys <jiginkarat03@gmail.com>
2024-10-01 18:53:07 +0530

    integration

---
+++ weather_details/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    'name': 'Weather Details',
+    'version': '17.0.1.0.0',
+    'sequence': -1,
+    'summary': "",
+    'description': " ",
+    'depends': ['base'],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'weather_details/static/src/xml/weather.xml',
+        ]
+    },
+
+    'license': 'LGPL-3',
+}
