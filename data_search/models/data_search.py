eee4cf1ca720cd6e9cb50447c4ffb3c69cd80cbc
cybrosys <jiginkarat03@gmail.com>
2024-10-17 21:11:50 +0530

    dashboard

--- data_search/models/data_search.py
+++ data_search/models/data_search.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-from odoo import api,fields,models
+from odoo import api, fields, models
+
 
 class DataSearch(models.Model):
     _name = 'data.search'
     _description = "Data Search"
 
-
-    name = fields.Char('Search Text')
-    model_id = fields.Many2one('ir.model','Model Name')
-    field_id = fields.Many2one('ir.model.fields','Field Name')
-    records = fields.One2many('search.result', 'search_id', string='Fields', required=True, copy=True)
+    name = fields.Char('Search Text', required=True, search='_search_record')
+    model_id = fields.Many2one('ir.model', 'Model Name')
+    field_id = fields.Many2one('ir.model.fields', 'Field Name')
+    records = fields.One2many('search.result', 'search_id', string='Fields',
+                              required=True, copy=True)
 
     @api.onchange('model_id')
     def get_fields(self):
-        print("fieldss")
         if self.model_id:
             domain = [('model_id.id', '=', self.model_id.id)]
         else:
@@ -21,20 +21,67 @@ class DataSearch(models.Model):
         return {'domain': {'field_id': domain}}
 
     def search_content(self):
-        if self.name and self.model_id and self.field_id:
-            Model = self.env[self.model_id.model]
-            field_name = self.field_id.name
+        if self.name:
+            self.records = [
+                fields.Command.clear()]
 
-            results = Model.search([(field_name, 'ilike', self.name)])
-            print(results)
+            if self.model_id and self.field_id:
+                Model = self.env[self.model_id.model]
+                field_name = self.field_id.name
+                results = Model.search([(field_name, 'ilike', self.name)])
 
-            self.records = [fields.Command.clear()]
-            for rec in results:
-                print(rec[field_name])
-                print(rec.id)
-                self.env['search.result'].create({
-                    'search_id': self.id,
-                    'name': rec.name,
-                    'record_id': rec.id,
-                })
-        return True
+                for rec in results:
+                    print(rec.id)
+
+                    self.env['search.result'].create({
+                        'search_id': self.id,
+                        'name': rec.name,
+                        'create_date': rec.create_date,
+                        'record_id': rec.id,
+                        'model_name': self.model_id.model,
+                        'field_name': field_name
+                    })
+
+            elif self.model_id and not self.field_id:
+                Model = self.env[self.model_id.model]
+                text_fields = self.env['ir.model.fields'].search(
+                    [('model_id', '=', self.model_id.id), ('store', '=', True)])
+                for field in text_fields:
+                    results = Model.search([(field.name, 'ilike', self.name)])
+                    for rec in results:
+                        self.env['search.result'].create({
+                            'search_id': self.id,
+                            'name': rec.name,
+                            'record_id': rec.id,
+                            'create_date': rec.create_date,
+                            'model_name': self.model_id.model,
+                            'field_name': field.name
+                        })
+
+
+            else:
+                models = self.env['ir.model'].sudo().search([])
+                for model in models:
+                    if model.id:
+                        # print(model)
+                        Model = self.env[model.model]
+                        print(Model)
+                        text_fields = self.env['ir.model.fields'].sudo().search(
+                        [('model_id', '=', model.id)])
+                        for field in text_fields:
+                            print(field.name)
+                        #     results = Model.sudo().search(
+                        #         [])
+                        #     print(results)
+                        # for rec in results:
+                        #     vals = {
+                        #         'search_id': self.id,
+                        #         'name': rec.name,
+                        #         'create_date': rec.create_date
+                        #     }
+                        #     # if rec.id:
+                        #     #     vals['record_id']= rec.id
+                        #     self.env['search.result'].create(vals)
+                        #     break
+
+            return True
--- data_search/models/search_result.py
+++ data_search/models/search_result.py
@@ -7,3 +7,15 @@ class SearchResult(models.Model):
     search_id = fields.Many2one("data.search", "Search ID")
     name = fields.Char('Result')
     record_id = fields.Integer('Record ID')
+    model_id = fields.Integer('Model ID')
+    model_name = fields.Char('Model Name')
+    field_name = fields.Char('Field Name')
+
+    def redirect_to_model(self):
+        print(self.record_id)
+        return  {'type': 'ir.actions.act_window',
+         'name': 'Related Record',
+         'res_model': self.model_name,
+         'view_mode': 'form',
+         'res_id':self.record_id,
+        'target': 'current'}
